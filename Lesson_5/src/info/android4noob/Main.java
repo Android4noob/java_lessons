9c2a1e62e69b3eb92534a246788efd12a87759a2
Android4noobs <junior.developer.notes@gmail.com>
2016-10-24 13:00:11 +0300

    add_java_lesson_5

--- Lesson_5/src/info/android4noob/Main.java
+++ Lesson_5/src/info/android4noob/Main.java
@@ -3,47 +3,46 @@ package info.android4noob;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i);
-            //0,1,2,3,4,5,6,7,8,9
-        }
-        System.out.print("\n");
-        for (int i = 0; i <= 10; i++) {
-            System.out.print(i);
-            //0,1,2,3,4,5,6,7,8,9,10
-        }
+        int i = 10;
+        byte b = 10;
+        short s = 10;
+        float f = 10f;
+        long l = 10L;
+        double d = 10d;
+        char c = 'c';
+        String string = "100";
+
+
+        System.out.println("Преобразование byte в int" + " " + (i = b));
+        System.out.println("Преобразование int в byte" + " " + (b = (byte) i));
+
+        System.out.println("Преобразование short в int" + " " + (i = s));
+        System.out.println("Преобразование int в short" + " " + (s = (short) i));
+
+        System.out.println("Преобразование int в float" + " " + (f = i));
+        System.out.println("Преобразование float в int" + " " + (i = (int) f));
+
+        System.out.println("Преобразование int в double" + " " + (d = i));
+        System.out.println("Преобразование double в int" + " " + (i = (int) d));
+
+        System.out.println("Преобразование int в long" + " " + (l = i));
+        System.out.println("Преобразование long в int" + " " + (i = (int) l));
 
         System.out.print("\n");
+        System.out.println("Преобразование int в String" + " " + String.valueOf(i));
+        System.out.println("Преобразование byte в String" + " " + String.valueOf(b));
+        System.out.println("Преобразование short в String" + " " + String.valueOf(s));
+        System.out.println("Преобразование long в String" + " " + String.valueOf(l));
+        System.out.println("Преобразование float в String" + " " + String.valueOf(f));
+        System.out.println("Преобразование double в String" + " " + String.valueOf(d));
+        System.out.println("Преобразование char в String" + " " + String.valueOf(c));
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i);
-            //1,2,3,4,5,6,7,8,9,10
-        }
-
         System.out.print("\n");
-        for (int i = 10; i >= 1; i--) {
-            System.out.print(i);
-            //10,9,8,7,6,5,4,3,2,1
-        }
-        System.out.print("\n");
-        for (int i = 10; i > 0; i--) {
-            System.out.print(i);
-            //10,9,8,7,6,5,4,3,2,1
-        }
-        System.out.print("\n");
-        for (int i = 10; i >= 0; i--) {
-            System.out.print(i);
-            //10,9,8,7,6,5,4,3,2,1,0
-        }
-        System.out.print("\n");
-        for (int i = 0; i < 10; i += 2) {
-            System.out.print(i);
-            //0,2,4,6,8
-        }
-        System.out.print("\n");
-        for (int i = 0; i < 10; i += 3) {
-            System.out.print(i);
-            //0,3,6,9
-        }
-    }
+        System.out.println("Преобразование String в int" + " " + Integer.parseInt(string));
+        System.out.println("Преобразование String в byte" + " " + Byte.parseByte(string));
+        System.out.println("Преобразование String в short" + " " + Short.parseShort(string));
+        System.out.println("Преобразование String в float" + " " + Float.parseFloat(string));
+        System.out.println("Преобразование String в double" + " " + Double.parseDouble(string));
+        System.out.println("Преобразование String в long" + " " + Long.parseLong(string));
+     }
 }
